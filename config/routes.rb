5eb5e8b0fe3cde0f9a74ac6120130778f838afc3
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-16 09:55:28 -0700

    Adding route to save to database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
 end
