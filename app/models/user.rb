7c9e25276ee714c4a16dab8d3807099b762c6580
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-16 11:05:14 -0700

    Adding post model and seeds for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts
-  has_many :comments
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, dependent: :destroy
   has_many :likes
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@ emails = ['eli@woof.com', 'santiago@woof.com', 'dulce@woof.com']
   user = User.create(name: Faker::Name.unique.first_name,
                      lastname: Faker::Name.last_name,
                      email: emails[i],
-                    password: 'croquetas',
-                    password_confirmation: 'croquetas')
+                     password: 'croquetas',
+                     password_confirmation: 'croquetas')
+
+  5.times do
+    user.posts.build(
+      content: Faker::Hacker.say_something_smart,
+    ).save
   end
+end
