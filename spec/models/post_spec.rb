ff4d694f590b137fc8e4d81f19ff3eba16a0bd04
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-19 15:52:26 -0700

    Testing belongs_to assc in Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let (:author) { User.new(:name => "Dulce", :lastname => 'Woof', :email => "dulce@woof.com", :password => "123456", :password_confirmation => "123456") }
   subject { Post.new }
+  
   describe 'basic validations for post model' do
     it 'is not valid without content' do
       expect(subject).not_to be_valid
@@ -21,6 +23,12 @@ RSpec.describe Post, type: :model do
   end
 
   describe 'validating correct associations' do
+    subject { Post.new(:author => author) }
+
+    it "belongs to User" do 
+      should belong_to(:author) 
+    end
+
     it "has many comments" do
     assc = described_class.reflect_on_association(:comments)
     expect(assc.macro).to eq :has_many
