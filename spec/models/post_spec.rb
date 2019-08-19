9bf93a35d621229351c4efbefdea8c177974ab82
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-19 15:03:40 -0700

    Adding tests for post associations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,18 +4,32 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   subject { Post.new }
+  describe 'basic validations for post model' do
+    it 'is not valid without content' do
+      expect(subject).not_to be_valid
+    end
 
-  it 'is not valid without content' do
-    expect(subject).not_to be_valid
+    it 'is not valid if it has more than 500 char' do
+      subject.content = 'a' * 501
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid if it has proper length data but no author' do
+      subject.content = 'a' * 499
+      expect(subject).not_to be_valid
+    end
   end
 
-  it 'is not valid if it has more than 500 char' do
-    subject.content = 'a' * 501
-    expect(subject).not_to be_valid
+  describe 'validating correct associations' do
+    it "has many comments" do
+    assc = described_class.reflect_on_association(:comments)
+    expect(assc.macro).to eq :has_many
+    end
+
+    it "has many likes" do
+    assc = described_class.reflect_on_association(:likes)
+    expect(assc.macro).to eq :has_many
+    end
   end
 
-  it 'is not valid if it has proper length data but no author' do
-    subject.content = 'a' * 499
-    expect(subject).not_to be_valid
-  end
 end
