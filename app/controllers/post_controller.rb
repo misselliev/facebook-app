22cd2d04a255ca9e6a46334a630095919aa83375
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-21 11:55:40 -0700

    Adding post controller, setting up routes and basic views

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,20 @@
+class PostController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :author_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   get 'pages/aqui'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
+  
+  resources :users, only: %i[new create index]
+  get 'user_index', to: 'users#index'
+  
+  resources :posts, only: %i[new create index]
+  get 'post_index', to: 'post#index'
+
 end
