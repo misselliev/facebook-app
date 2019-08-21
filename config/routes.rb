41c24f48427eb648286c17c6e460ed57ce2d8688
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-21 12:12:10 -0700

    Adding users index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   
   resources :users, only: %i[new create index]
-  get 'user_index', to: 'users#index'
+  get 'users_index', to: 'users#index'
   
   resources :posts, only: %i[new create index]
   get 'posts_index', to: 'posts#index'
