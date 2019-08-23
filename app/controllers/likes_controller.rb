5e89f91faa13b48d7a5c6ad55216293925bba905
Santiago Torres G <thdealer@gmail.com>
2019-08-23 16:29:57 -0500

    customizing redirection for liking action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,8 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.build(post_id: params[:id])
     if @like.save
-      redirect_to posts_index_path
+      redirect_to posts_index_path if params[:from] === 'index'
+      redirect_to post_path if params[:from] === 'show'
     else
       flash[:alert] = "Something went wrong"
     end
