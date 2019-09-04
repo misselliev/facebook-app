159b82e681db3bcbe4ab62c159e8f3c57dbbdce5
Santiago Torres G <thdealer@gmail.com>
2019-09-04 17:52:30 -0500

    friendship factories added

--- spec/factories/friendship.rb
+++ spec/factories/friendship.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user, aliases: [:author] do
-    trait :valid_user do
-      id { 1 }
-      name { 'Dulce' }
-      lastname { 'Woof' }
-      email { Faker::Internet.email }
-      password { '123456' }
+  factory :friendship do
+    user1 = Factory.build(:user)
+    user2 = Factory.build(:user)
+    user3 = Factory.build(:user)
+    user4 = Factory.build(:user)
+
+    trait :confirmed do
+      user_id { user1.id }
+      friend_id { user2.id }
+      confirmed { true }
     end
 
-    trait :invalid_user do
-      id { nil }
-      name { 'Li' }
-      lastname { 'Eu' }
-      email { 'email .net' }
-      password { '123' }
+    trait :rejected do
+      user_id { user1.id }
+      friend_id { user3.id }
+      confirmed { false }
+    end
+
+    trait :pending do
+      user_id { user1.id }
+      friend_id { user4.id }
     end
   end
 end
