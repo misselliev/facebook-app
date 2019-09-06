4cdbca24e17337736ced8ad483aa76aee0dbc85e
Santiago Torres G <thdealer@gmail.com>
2019-09-06 17:23:22 -0500

    fixing issues with likes from user profile and updating partiall for _display_post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,21 +2,29 @@ class LikesController < ApplicationController
   before_action :authorized?
   def create
     @like = current_user.likes.build(post_id: params[:id])
-    redirecting_data(@like.save)
-
+    if params[:from] == 'user'
+      redirecting_data(@like.save, params[:user])
+    else
+      redirecting_data(@like.save)
+    end
   end
 
   def destroy
     @like = current_user.likes.find_by(post_id: params[:id])
-    redirecting_data(@like.delete)
+    if params[:from] == 'user'
+      redirecting_data(@like.delete, params[:user])
+    else
+      redirecting_data(@like.delete)
+    end
   end
 
-  private 
+  private
 
-  def redirecting_data(param)
+  def redirecting_data(param, user = nil)
     if param
       redirect_to posts_index_path if params[:from] === 'index'
       redirect_to post_path if params[:from] === 'show'
+      redirect_to user_path(user) if params[:from] === 'user'
     else
       flash[:alert] = "Something went wrong"
     end
