1a8eda066b0a83ed61f3c6e4ae8a7dee813499f2
Santiago Torres G <thdealer@gmail.com>
2019-09-10 18:18:21 -0500

    halt cycle error fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,23 +30,22 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update_attributes(post_params)
-      flash[:success] = 'Post updated'
-      redirect_to post_path(params[:id])
+      flash[:notice] = 'Post updated'
+      render post_path(params[:id])
     else
       render 'edit'
     end
-    
   end
 
   private
 
   def access_post
     post = current_user.posts.find_by(id: params[:id])
-    if post
-      redirect_to edit_post_path(post)
-    else
-      flash[:alert] = "You are not authorized to do this" 
+    if post.nil?
+      flash[:alert] = "You are not authorized to do this"
       redirect_to post_path(params[:id])
+    else
+      @post
     end
   end
 
