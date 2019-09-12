13ebb97d6fb09c85ba275bee9a7836b4c16939f2
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-09-12 11:12:02 -0700

    refactoring show method variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
-    @joined_at = @user.created_at.year
-    @num_friends = @user.friendships.confirmed.count
-    @name = "#{@user.name} #{@user.lastname}"
-    @email = @user.email
     @is_a_friend = current_user.friendships.find_by_friend_id(@user)
     @diff_profile_new_friend = @user != current_user && @is_a_friend.nil?
     @pending_requests = Friendship.pending_with_friend_id(current_user)
-    @friends = @user.friendships.confirmed
   end
 
   def create_friendship
