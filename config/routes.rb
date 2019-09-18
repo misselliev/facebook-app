48c702f00b476787cc6875a1f9cf166a05e1a1b5
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-09-17 17:08:52 -0700

    Updating homepage according to whether user is authenticated or not

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
-  get 'friendships/create'
-  root 'pages#home'
+authenticated :user do
+  root :to => 'posts#index', as: :authenticated_root
+end
+root :to => 'pages#home'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  
-  resources :users, only: %i[new create index show]
-  get 'users_index', to: 'users#index'
-  
-  resources :posts, only: %i[new create index show edit update]
-  get 'posts_index', to: 'posts#index'
-  match '/posts/create', to: 'posts#create', via: :post, as: :create_post
-  match '/likes/create/:id', to: 'likes#create', via: :post, as: :create_like
-  delete '/likes/destroy/:id', to: 'likes#destroy', as: :destroy_like
+devise_for :users, :controllers => { registrations: 'registrations' }
 
-  get 'comments/new'
-  match '/comments/create/:id', to: 'comments#create', via: :post, as: :create_comment
+resources :users, only: %i[new create index show]
+get 'users_index', to: 'users#index'
 
-  resources :friendships, only: %i[create destroy]
-  match '/friendships/update_status/:id', to: 'friendships#update_status', via: :post, as: :update_status
+resources :posts, only: %i[new create index show edit update]
+get 'posts_index', to: 'posts#index'
+match '/posts/create', to: 'posts#create', via: :post, as: :create_post
+match '/likes/create/:id', to: 'likes#create', via: :post, as: :create_like
+delete '/likes/destroy/:id', to: 'likes#destroy', as: :destroy_like
+
+get 'comments/new'
+match '/comments/create/:id', to: 'comments#create', via: :post, as: :create_comment
+
+resources :friendships, only: %i[create destroy]
+match '/friendships/update_status/:id', to: 'friendships#update_status', via: :post, as: :update_status
+get 'friendships/create'
 end
