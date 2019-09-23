05a02ad55620d2cf736b133a1e6d37a42c23b916
Santiago Torres G <thdealer@gmail.com>
2019-09-23 12:59:34 -0500

    fixing index in name

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name.split(' ')[1]
+      user.name = auth.info.name.split(' ')[0]
       user.lastname = auth.info.name.split[1..-1].join(' ')
       user.image = auth.info.image
       user.save!
