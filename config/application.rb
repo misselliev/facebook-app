87d53c6400a7062f72b2f15b48f367489a71ec2b
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-09-23 13:39:07 -0700

    Adding envs to app

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, 'APP_ID', 'APP_SECRET', callback_url: 'https://your_url/users/auth/facebook/callback/'
+  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'], callback_url: ENV['CALLBACK_URL'] 
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
