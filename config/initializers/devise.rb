030c45ef0a89ebffed89886ada3ad9a5c018628e
Santiago Torres G <thdealer@gmail.com>
2019-09-23 12:58:08 -0500

    modifiyng oauth fb settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, '1343982042444839', '5a16e9fa86d64b867b6e546610548270', callback_url: 'https://b7c1d79c.ngrok.io/users/auth/facebook/callback'
+  config.omniauth :facebook, '1343982042444839', '5a16e9fa86d64b867b6e546610548270', callback_url: 'http://localhost:3000/users/auth/facebook/callback/'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
